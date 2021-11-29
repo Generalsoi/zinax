344b9c54bed96087aa34b1d9e3c446612dd68d69
Generalsoi <ikhinobelesuccess@gmail.com>
2021-11-29 23:57:39 +0100

    updates

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -7,7 +7,41 @@ const Roadmap = () => {
     <section className="roadmap">
       <h3>Road Map</h3>
       <div className="roadmap-content">
-        <img src={RoadMap} alt="roadmap" />
+        <div>
+          <div className="quarter-div">
+            <div className="quarter">
+              <p>Q3 21</p>
+            </div>
+          </div>
+          <p>Ecosystem building, Farming and staking pools</p>
+        </div>
+        <div>
+          <p>IDO, Project, CEX listing commences</p>
+          <div className="quarter-div">
+            <div className="quarter">
+              <p>Q1 22</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="quarter-div">
+            <div className="quarter">
+              <p>Q2 22</p>
+            </div>
+          </div>
+          <p>$100 million, worth of transaction volume on Zinax.</p>
+        </div>
+        <div>
+          <p>
+            40,000 holders, 200,000 users, transaction worth $400 million on
+            Zinax
+          </p>
+          <div className="quarter-div">
+            <div className="quarter">
+              <p>Q3 22</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
