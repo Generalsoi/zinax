76028d411157f7f4b5bb28b7754d8a62cd3d133b
Generalsoi <ikhinobelesuccess@gmail.com>
2021-12-01 12:27:08 +0100

    updates to the zinax site

--- src/components/roadmap.jsx
+++ src/components/roadmap.jsx
@@ -7,33 +7,36 @@ const Roadmap = () => {
     <section className="roadmap">
       <h3>Road Map</h3>
       <div className="roadmap-content">
-        <div>
+        <div className="roadmap-content-div first-roadmap-div">
           <div className="quarter-div">
             <div className="quarter odd">
               <p>Q3 21</p>
             </div>
           </div>
-
-          <p>Ecosystem building, Farming and staking pools</p>
+          <p className="quarter-p">
+            Ecosystem building, Farming and staking pools
+          </p>
         </div>
-        <div>
-          <p>IDO, Project, CEX listing commences</p>
+        <div className="roadmap-content-div second-roadmap-div">
+          <p className="quarter-p">IDO, Project, CEX listing commences</p>
           <div className="quarter-div">
             <div className="quarter even">
               <p>Q1 22</p>
             </div>
           </div>
         </div>
-        <div>
+        <div className="roadmap-content-div third-roadmap-div">
           <div className="quarter-div">
             <div className="quarter odd">
               <p>Q2 22</p>
             </div>
           </div>
-          <p>$100 million, worth of transaction volume on Zinax.</p>
+          <p className="quarter-p">
+            $100 million, worth of transaction volume on Zinax.
+          </p>
         </div>
-        <div>
-          <p>
+        <div className="roadmap-content-div last-roadmap-div">
+          <p className="quarter-p">
             40,000 holders, 200,000 users, transaction worth $400 million on
             Zinax
           </p>
