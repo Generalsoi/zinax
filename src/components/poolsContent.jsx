31f67b5d363b5e97bdf0d121fca7d9dca7e2f794
Generalsoi <ikhinobelesuccess@gmail.com>
2021-12-07 13:46:34 +0100

    updates

--- src/components/poolsContent.jsx
+++ src/components/poolsContent.jsx
@@ -9,6 +9,36 @@ import BinanceLogo from "../assets/images/binance-logo.png";
 
 const PoolsContent = () => {
   const [viewState, setViewState] = useState(1);
+  const dummyData = [
+    {
+      tokenNames: "ZINAX-BNB",
+      tokenImg1: ZinaxLogo,
+      tokenImg2: BinanceLogo,
+      multiplier: "40X",
+      aprValue: "2.72%",
+      earnedValue: "120",
+      liquidityValue: "$780,344",
+    },
+    {
+      tokenNames: "BUSD-BNB",
+      tokenImg1: ZinaxLogo,
+      tokenImg2: BinanceLogo,
+      multiplier: "50X",
+      aprValue: "2.95%",
+      earnedValue: "145",
+      liquidityValue: "$980,354",
+    },
+    {
+      tokenNames: "ZINAX-USDT",
+      tokenImg1: ZinaxLogo,
+      tokenImg2: BinanceLogo,
+      multiplier: "60X",
+      aprValue: "4.72%",
+      earnedValue: "198",
+      liquidityValue: "$990,433",
+    },
+  ];
+
   return (
     <div className="pools-content">
       <div className="pools-header-content">
@@ -47,28 +77,55 @@ const PoolsContent = () => {
         </div>
       </div>
 
-      <div className={`pools-body ${viewState ? "" : ""}`}>
-        {viewState ? (
-          <PoolsCard
-            tokenNames="ZINAX-BNB"
-            tokenImg1={ZinaxLogo}
-            tokenImg2={BinanceLogo}
-            multiplier="40X"
-            aprValue="2.72%"
-            earnedValue="120"
-            liquidityValue="$780,344"
-          />
-        ) : (
-          <PoolsAccordion
-            tokenNames="ZINAX-BNB"
-            tokenImg1={ZinaxLogo}
-            tokenImg2={BinanceLogo}
-            multiplier="40X"
-            aprValue="2.72%"
-            earnedValue="120"
-            liquidityValue="$780,344"
-          />
-        )}
+      <div
+        className={`pools-body ${
+          viewState ? "pools-card-view" : "pools-accordion-view"
+        }`}
+      >
+        {viewState
+          ? dummyData.map((data) => (
+              <PoolsCard
+                tokenNames={data.tokenNames}
+                tokenImg1={data.tokenImg1}
+                tokenImg2={data.tokenImg2}
+                multiplier={data.multiplier}
+                aprValue={data.aprValue}
+                earnedValue={data.earnedValue}
+                liquidityValue={data.liquidityValue}
+              />
+            ))
+          : // <PoolsCard
+            //   tokenNames="ZINAX-BNB"
+            //   tokenImg1={ZinaxLogo}
+            //   tokenImg2={BinanceLogo}
+            //   multiplier="40X"
+            //   aprValue="2.72%"
+            //   earnedValue="120"
+            //   liquidityValue="$780,344"
+            // />
+
+            dummyData.map((data) => (
+              <PoolsAccordion
+                tokenNames={data.tokenNames}
+                tokenImg1={data.tokenImg1}
+                tokenImg2={data.tokenImg2}
+                multiplier={data.multiplier}
+                aprValue={data.aprValue}
+                earnedValue={data.earnedValue}
+                liquidityValue={data.liquidityValue}
+              />
+            ))
+
+            // <PoolsAccordion
+            //   tokenNames="ZINAX-BNB"
+            //   tokenImg1={ZinaxLogo}
+            //   tokenImg2={BinanceLogo}
+            //   multiplier="40X"
+            //   aprValue="2.72%"
+            //   earnedValue="120"
+            //   liquidityValue="$780,344"
+            // />
+        }
       </div>
     </div>
   );
