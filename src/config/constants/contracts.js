0782828e85b353003cb3945feb21815795aa7519
CryptoNate <89227631+DeFiDruid@users.noreply.github.com>
2021-12-13 17:58:40 -0800

    new abis

---
+++ src/config/constants/contracts.js
@@ -0,0 +1,14 @@
+export default {
+  masterChef: {
+    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+    56: '0x9BC5e26bee164955992320d6cB2e1792c9e42473',
+  },
+  // sousChef: {
+  //  97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
+  //  56: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
+  // },
+  multiCall: {
+    56: '0xfF6FD90A470Aaa0c1B8A54681746b07AcdFedc9B',
+    97: '0x8F3273Fb89B075b1645095ABaC6ed17B2d4Bc576',
+  },
+}
